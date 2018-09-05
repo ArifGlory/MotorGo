a25639a50c96a58c4b51daf636404d4b97f0924b
unknown <arifglory46@gmail.com>
2018-09-05 11:41:14 +0700

    versi05092018

---
+++ app/src/main/java/Kelas/SharedVariable.java
@@ -0,0 +1,15 @@
+package Kelas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Glory on 04/09/2018.
+ */
+
+public class SharedVariable {
+    public static String nama = "Not Logged in";
+    public static int jmlMotor = 0;
+    public static String userID = "";
+    public static List<String> list_motor = new ArrayList();
+}
---
+++ app/src/main/java/hadi/motorhebat/BerandaActivity.java
@@ -0,0 +1,15 @@
+package hadi.motorhebat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import Kelas.BaseDrawerActivity;
+
+public class BerandaActivity extends BaseDrawerActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+    }
+}
